4031d20202360810b443a6bc955eac0dafb5a108
Shonelle Swarens <sswarens@hawaii.edu>
2015-12-08 09:09:22 -1000

    Add Tree
    With reset button

--- client/templates/trees/addTree.js
+++ client/templates/trees/addTree.js
@@ -1,3 +1,50 @@
 Template.addTree.rendered = function() {
 
 };
+Schemas = {};
+
+Template.registerHelper("Schemas", Schemas);
+
+Schemas.Person = new SimpleSchema({
+  treeName: {
+    type: String,
+    index: 1,
+    unique: true
+  },
+  location: {
+    type: String,
+    optional: true
+  },
+  latitude: {
+    type: Number,
+    optional: true
+  },
+  longitude: {
+    type: Number,
+    optional: true
+  },
+  diameter: {
+    type: Number,
+    optional: true
+  }
+});
+
+var Collections = {};
+
+Template.registerHelper("Collections", Collections);
+
+People = Collections.People = new Mongo.Collection("People");
+People.attachSchema(Schemas.Person);
+
+Meteor.publish(null, function () {
+  return People.find();
+});
+
+People.allow({
+  insert: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
